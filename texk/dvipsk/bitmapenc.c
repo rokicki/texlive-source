c6af0c1f285ecb55ad28050f738c5ead715b60c6
rokicki <rokicki@gmail.com>
2019-07-15 09:17:12 -0700

    Removed unnecessary bitmapenc.c.

--- texk/dvipsk/bitmapenc.c
+++ texk/dvipsk/bitmapenc.c
@@ -38,7 +38,6 @@
 #endif
 #include "dvips.h"
 #include "protos.h"
-#include "bitmapenc.h"
 /*
  *   Do we use this functionality?  If 0, then no; if 1, then yes, but
  *   suppress warnings; if 2, then warn.
@@ -56,6 +55,25 @@ void bitmapencopt(int v) {
 #endif
 #define ENCODING_CHAR_COUNT 256
 #define MAX_LINE_LENGTH 256
+/*
+ *   A structure for storing deduplicated encodings.  These are deduplicated
+ *   in dvips memory on load.  We also store information on whether the
+ *   encoding has been downloaded in a particular section.
+ */
+struct bmenc {
+   const char **enc ;   // the encoding itself
+   int downloaded_seq ; // -1: not downloaded; else, sequence number
+   struct bmenc *next ; // next encoding in linear linked list
+} ;
+/*
+ *   This structure maintains a relationship between a font name and a
+ *   bitmap encoding.  Again, linear search is used.
+ */
+struct bmfontenc {
+   const char *fontname ;   // dvi font name
+   struct bmenc *enc ;      // the encoding to use
+   struct bmfontenc *next ; // next encoding
+} ;
 /*
  *   Parse the encoding file.  If use_all flag is set then we are parsing
  *   the dvips_all file which can have multiple encodings, each preceded
